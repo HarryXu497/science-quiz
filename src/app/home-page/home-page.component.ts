d28593f821be3e2877a0d189241b9a99b37642a4
HarryXu497 <harryxu497@gmail.com>
2022-10-26 15:22:56 -0400

    add service to manage app metadata

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MetadataService } from '../metadata.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent { }
+export class HomePageComponent { 
+	constructor(public meta: MetadataService) { }
+}
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MetadataService } from '../metadata.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent { }
+export class NavbarComponent { 
+	constructor(public meta: MetadataService) { }
+}
