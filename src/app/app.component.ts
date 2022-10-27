ca761465244efa5012d21dcac85ff30a75fa27b5
HarryXu497 <harryxu497@gmail.com>
2022-10-27 15:08:32 -0400

    removed some useless code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,24 +12,5 @@ import { QuestionsService } from './questions.service';
   ]
 })
 export class AppComponent {
-	constructor(public router: Router, private qs: QuestionsService) { }
-
-	onClick() {
-		this.qs.addBalancingChemicalEquationQuestion(QuestionsService.createBalancingChemicalEquationQuestion({
-			explaination: `
-				It works!!`,
-			tags: ["chemistry", "balancing-equations"],
-			subject: "chemistry",
-			answers: [2, 3, 2],
-			question: "What is the balanced equation of $Fe + Cl_2 \\rightarrow FeCl_3$?",
-			reactants: [
-				"$Fe$",
-				"$Cl_2$",
-			],
-			products: [
-				"$FeCl_3$"
-			],
-			reactionType: 'yields'
-		}))
-	}
+	constructor(public router: Router) { }
 }
